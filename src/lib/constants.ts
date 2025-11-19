0cdef9732ca539546436e23493801a8bca8bc151
Shanka Visal Amarasekara <shankavisal@gmail.com>
2025-11-19 09:23:34 +0000

    This is the location - https://maps.app.goo.gl/Kyv82aThpxMFkna67

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
+
 export const BOOKING_COM_URL = 'https://www.booking.com';
 export const AGODA_URL = 'https://www.agoda.com';
 export const AIRBNB_URL = 'https://www.airbnb.com';
@@ -5,7 +6,7 @@ export const AIRBNB_URL = 'https://www.airbnb.com';
 export const WHATSAPP_PHONE_NUMBER = '+94000000000'; // Replace with a real number
 export const WHATSAPP_MESSAGE = 'Hello! I would like to inquire about booking a room at Vercour Sigiriya.';
 
-export const HOTEL_LOCATION_GMAPS_URL = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.111838634863!2d80.7589573152636!3d7.77800799439611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afca7a71f768c75%3A0x9d35b7a5146c3d8e!2sSigiriya!5e0!3m2!1sen!2slk!4v1678886361234!5m2!1sen!2slk';
+export const HOTEL_LOCATION_GMAPS_URL = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.159049463943!2d80.7445213760925!3d7.772922192237889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afca7a1c81ace5b%3A0x248a31398e18512!2sVercour%20Sigiriya!5e0!3m2!1sen!2slk!4v1721860228389!5m2!1sen!2slk';
 
 export const HOTEL_EMAIL = 'reservations@vercour.com';
 export const HOTEL_PHONE = '+94 11 2 345 678';
