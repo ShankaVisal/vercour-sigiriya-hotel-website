dc723e80937309a03d282b2b265ea6a1120c07d1
Shanka Visal Amarasekara <shankavisal@gmail.com>
2025-11-19 09:24:36 +0000

    7°56'02.5"N 80°42'31.5"E

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,8 +6,9 @@ export const AIRBNB_URL = 'https://www.airbnb.com';
 export const WHATSAPP_PHONE_NUMBER = '+94000000000'; // Replace with a real number
 export const WHATSAPP_MESSAGE = 'Hello! I would like to inquire about booking a room at Vercour Sigiriya.';
 
-export const HOTEL_LOCATION_GMAPS_URL = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.159049463943!2d80.7445213760925!3d7.772922192237889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afca7a1c81ace5b%3A0x248a31398e18512!2sVercour%20Sigiriya!5e0!3m2!1sen!2slk!4v1721860228389!5m2!1sen!2slk';
+export const HOTEL_LOCATION_GMAPS_URL = 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3951.849615598683!2d80.706175!3d7.909583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwNTYnMDQuMiJOIDgwwrA0MiczMS41IkU!5e0!3m2!1sen!2sus!4v1724097482531!5m2!1sen!2sus';
 
 export const HOTEL_EMAIL = 'reservations@vercour.com';
 export const HOTEL_PHONE = '+94 11 2 345 678';
 export const HOTEL_ADDRESS = 'Vercour Sigiriya, Sigiriya, Sri Lanka';
+
